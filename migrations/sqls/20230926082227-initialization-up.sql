62718596df2a5284aca7f2774a2ca9cb166ef1fe
mukti222 <135799527+mukti222@users.noreply.github.com>
2023-09-26 21:59:39 +0700

    Add files via upload

---
+++ migrations/sqls/20230926082227-initialization-up.sql
@@ -0,0 +1,14 @@
+/* Replace with your SQL commands */
+ALTER TABLE ACTOR
+ADD COLUMN AGE INTEGER;
+
+
+/* TESTING:
+ db-migrate up
+received data: /* Replace with your SQL commands */
+ALTER TABLE ACTOR
+ADD COLUMN AGE INTEGER;
+[INFO] Processed migration 20230926082227-initialization
+[INFO] Done
+PS A:\tugas\devs\september\hw8-haryomukti> 
+*/
